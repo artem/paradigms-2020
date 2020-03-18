5f0e2ff100cd3379d117ff0123502a8d5a6ee992
Artem Labazov <123321artyom@gmail.com>
2020-03-18 22:56:39 +0300

    HW6: Inline

--- javascript/jstest/functional/functionalExpression.js
+++ javascript/jstest/functional/functionalExpression.js
@@ -1,10 +1,5 @@
 "use strict";
 
-const addImpl = (v1, v2) => v1 + v2;
-const subImpl = (v1, v2) => v1 - v2;
-const mulImpl = (v1, v2) => v1 * v2;
-const divImpl = (v1, v2) => v1 / v2;
-
 const cnst = value => x => value;
 const variable = () => x => x;
 const parse = input => input.trim() === "x" ? variable("x") : cnst(+input);
@@ -12,7 +7,7 @@ const parse = input => input.trim() === "x" ? variable("x") : cnst(+input);
 const negate = f1 => x => -f1(x);
 
 const binaryOp = op => (f1, f2) => x => op(f1(x), f2(x));
-const add = binaryOp(addImpl);
-const subtract = binaryOp(subImpl);
-const multiply = binaryOp(mulImpl);
-const divide = binaryOp(divImpl);
+const add = binaryOp((v1, v2) => v1 + v2);
+const subtract = binaryOp((v1, v2) => v1 - v2);
+const multiply = binaryOp((v1, v2) => v1 * v2);
+const divide = binaryOp((v1, v2) => v1 / v2);
