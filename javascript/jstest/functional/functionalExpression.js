03395d8e69fa3386e4b422c6dc780e16994fb797
Artem Labazov <123321artyom@gmail.com>
2020-03-19 16:54:19 +0300

    HW6: hard

--- javascript/jstest/functional/functionalExpression.js
+++ javascript/jstest/functional/functionalExpression.js
@@ -1,13 +1,34 @@
 "use strict";
 
-const cnst = value => x => value;
-const variable = () => x => x;
+const cnst = value => () => value;
+const pi = cnst(Math.PI);
+const e = cnst(Math.E);
+
+const variable = name => {
+    return (x, y, z) => {
+        switch (name) {
+            case 'x':
+                return x;
+            case 'y':
+                return y;
+            case 'z':
+                return z;
+        }
+    }
+};
+
 const parse = input => input.trim() === "x" ? variable("x") : cnst(+input);
 
-const negate = f1 => x => -f1(x);
+const negate = f1 => (x, y, z) => -f1(x, y, z);
 
-const binaryOp = op => (f1, f2) => x => op(f1(x), f2(x));
+const binaryOp = op => (f1, f2) => (x, y, z) => op(f1(x, y, z), f2(x, y, z));
+const ternaryOp = op => (f1, f2, f3) => (x, y, z) => op(f1(x, y, z), f2(x, y, z), f3(x, y, z));
+const quinaryOp = op => (f1, f2, f3, f4, f5) => (x, y, z) => op(f1(x, y, z), f2(x, y, z), f3(x, y, z), f4(x, y, z), f5(x, y, z));
+
 const add = binaryOp((v1, v2) => v1 + v2);
 const subtract = binaryOp((v1, v2) => v1 - v2);
 const multiply = binaryOp((v1, v2) => v1 * v2);
 const divide = binaryOp((v1, v2) => v1 / v2);
+
+const med3 = ternaryOp((v1, v2, v3) => Math.max(Math.min(v1, v2), Math.min(v1, v3), Math.min(v2, v3)));
+const avg5 = quinaryOp((v1, v2, v3, v4, v5) => (v1 + v2 + v3 + v4 + v5) / 5);
