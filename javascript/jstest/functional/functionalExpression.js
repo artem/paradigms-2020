38449eb945a6e2ea0755f5f2375a2cccbf99d787
Artem Labazov <123321artyom@gmail.com>
2020-03-25 20:35:22 +0300

    HW6: Generify functions

--- javascript/jstest/functional/functionalExpression.js
+++ javascript/jstest/functional/functionalExpression.js
@@ -21,14 +21,12 @@ const parse = input => input.trim() === "x" ? variable("x") : cnst(+input);
 
 const negate = f1 => (x, y, z) => -f1(x, y, z);
 
-const binaryOp = op => (f1, f2) => (x, y, z) => op(f1(x, y, z), f2(x, y, z));
-const ternaryOp = op => (f1, f2, f3) => (x, y, z) => op(f1(x, y, z), f2(x, y, z), f3(x, y, z));
-const quinaryOp = op => (f1, f2, f3, f4, f5) => (x, y, z) => op(f1(x, y, z), f2(x, y, z), f3(x, y, z), f4(x, y, z), f5(x, y, z));
+const operation = op => (...ops) => (x, y, z) => op(...(ops.map(value => value(x, y, z))));
 
-const add = binaryOp((v1, v2) => v1 + v2);
-const subtract = binaryOp((v1, v2) => v1 - v2);
-const multiply = binaryOp((v1, v2) => v1 * v2);
-const divide = binaryOp((v1, v2) => v1 / v2);
+const add = operation((v1, v2) => v1 + v2);
+const subtract = operation((v1, v2) => v1 - v2);
+const multiply = operation((v1, v2) => v1 * v2);
+const divide = operation((v1, v2) => v1 / v2);
 
-const med3 = ternaryOp((v1, v2, v3) => Math.max(Math.min(v1, v2), Math.min(v1, v3), Math.min(v2, v3)));
-const avg5 = quinaryOp((v1, v2, v3, v4, v5) => (v1 + v2 + v3 + v4 + v5) / 5);
+const med3 = operation((v1, v2, v3) => Math.max(Math.min(v1, v2), Math.min(v1, v3), Math.min(v2, v3)));
+const avg5 = operation((v1, v2, v3, v4, v5) => (v1 + v2 + v3 + v4 + v5) / 5);
