a6ec765f88d9d920450715049daa59408f6c218a
Artem Labazov <123321artyom@gmail.com>
2020-03-25 21:41:22 +0300

    HW7: Working parser

--- javascript/jstest/functional/functionalExpression.js
+++ javascript/jstest/functional/functionalExpression.js
@@ -17,16 +17,75 @@ const variable = name => {
     }
 };
 
-const parse = input => input.trim() === "x" ? variable("x") : cnst(+input);
-
-const negate = f1 => (x, y, z) => -f1(x, y, z);
-
 const operation = op => (...ops) => (x, y, z) => op(...(ops.map(value => value(x, y, z))));
 
+const negate = operation(v1 => -v1);
 const add = operation((v1, v2) => v1 + v2);
 const subtract = operation((v1, v2) => v1 - v2);
 const multiply = operation((v1, v2) => v1 * v2);
 const divide = operation((v1, v2) => v1 / v2);
 
-const med3 = operation((v1, v2, v3) => Math.max(Math.min(v1, v2), Math.min(v1, v3), Math.min(v2, v3)));
-const avg5 = operation((v1, v2, v3, v4, v5) => (v1 + v2 + v3 + v4 + v5) / 5);
+const avg = (...args) => [].reduce.call(args, (acc, cVal) => acc + cVal) / args.length;
+const med = (...args) => {
+    if (args.length === 0) {
+        return 0;
+    }
+
+    [].sort.call(args, (a, b) => a - b);
+    let half = Math.floor(args.length / 2);
+
+    if (args.length % 2) {
+        return args[half];
+    }
+    return (args[half - 1] + args[half]) / 2;
+};
+
+const med3 = operation((v1, v2, v3) => med(v1, v2, v3));
+const avg5 = operation((v1, v2, v3, v4, v5) => avg(v1, v2, v3, v4, v5));
+
+
+const OPERATIONS = {
+    '+': [add, 2],
+    '-': [subtract, 2],
+    '*': [multiply, 2],
+    '/': [divide, 2],
+    'negate': [negate, 1],
+    'avg5': [avg5, 5],
+    'med3': [med3, 3]
+};
+
+const VARS = ['x', 'y', 'z'];
+const CONSTANTS = {
+    'pi': pi,
+    'e': e
+};
+
+function parse(expr) {
+    let stack = [];
+    const tokens = expr.match(/\S+/g);
+    for (let token of tokens) {
+        parseToken(token, stack);
+    }
+    return stack.pop();
+}
+
+const parseToken = (token, stack) => {
+    let res;
+    const curOperation = OPERATIONS[token];
+    if (curOperation === undefined) {
+        if (VARS.includes(token)) {
+            res = variable(token);
+        } else if (token in CONSTANTS) {
+            res = CONSTANTS[token];
+        } else {
+            res = cnst(parseFloat(token));
+        }
+    } else {
+        let args = [];
+        for (let i = 0; i < curOperation[1]; i++) {
+            args.unshift(stack.pop());
+        }
+        res = curOperation[0](...args);
+    }
+    stack.push(res);
+};
