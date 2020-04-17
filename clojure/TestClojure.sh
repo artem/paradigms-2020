b21d668411d7672169b80bbba3116b2d187979f1
Georgiy Korneev <kgeorgiy@kgeorgiy.info>
2020-04-17 19:54:07 +0300

    Mac compatibility fix

--- clojure/TestClojure.sh
+++ clojure/TestClojure.sh
@@ -6,16 +6,20 @@ if [[ -z "$1" ]] ; then
     exit 1
 fi
 
+rpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
 TEST="$1"
-CLOJURE="$(dirname "$0")"
+CLOJURE="$(dirname $(rpath "$0"))"
 REPO="$(dirname "$CLOJURE")"
 LIB="$CLOJURE/lib"
 
 javac \
     -d "__out" \
     "--class-path=$LIB/*:$REPO/java:$REPO/javascript:$REPO/clojure" \
-    "$CLOJURE/${TEST//\./\/}.java"
+    "$CLOJURE/${TEST//\.//}.java"
 java \
     -ea \
     "--class-path=$LIB/*:__out" \
-    "$TEST" "$2"
+    "$TEST" "${2-}"
