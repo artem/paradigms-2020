fe8bc9dce588b162a051beb86e53d7648da05cd5
Artem Labazov <123321artyom@gmail.com>
2020-04-30 16:05:08 +0300

    HW10: hard modification, note about Clojure bug (and map->mapv, just in case Plotnikov disagrees about using map)

--- clojure/expression.clj
+++ clojure/expression.clj
@@ -1,3 +1,21 @@
+;
+; HW10 - hard (med/avg)
+;
+
+(defn average [& args]
+  (let [len (count args)
+        sum (reduce + args)]
+    (/ sum len)))
+
+(defn median [& args]
+  (let [len (count args)
+        sorted (sort args)]
+    (nth sorted (quot len 2))))
+
+; See https://clojure.atlassian.net/browse/CLJ-2244 for details
+(defn divideCljWorkaround [& args]
+  (reduce #(/ %1 (double %2)) args))
+
 (defn constant [x] (fn [_] x))
 
 (defn variable [x]
@@ -7,26 +25,31 @@
 (defn operation [f]
   (fn [& args]
     (fn [values]
-      (apply f (map #(%1 values) args)))))
+      (apply f (mapv #(%1 values) args)))))
 
 (def add (operation +))
 (def subtract (operation -))
 (def multiply (operation *))
-(def divide (operation #(/ %1 (double %2))))
+(def divide (operation divideCljWorkaround))
 (def negate (operation -))
+(def avg (operation average))
+(def med (operation median))
 
 (def variables {'x (variable "x")
                 'y (variable "y")
                 'z (variable "z")})
+
 (def ops {'+      add
           '-      subtract
           '*      multiply
           '/      divide
-          'negate negate})
+          'negate negate
+          'med    med
+          'avg    avg})
 
 (defn parseExpression [x]
   (cond
-    (list? x) (apply (ops (first x)) (map parseExpression (rest x)))
+    (list? x) (apply (ops (first x)) (mapv parseExpression (rest x)))
     (number? x) (constant x)
     (contains? variables x) (variables x)))
 
