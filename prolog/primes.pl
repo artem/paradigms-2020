066e27d6ce3e4a648ade2abab0cb3b18e6ec38c3
Artem Labazov <123321artyom@gmail.com>
2020-06-07 21:52:05 +0300

    hw13: format code

--- prolog/primes.pl
+++ prolog/primes.pl
@@ -9,29 +9,37 @@ prime(N) :- N > 1, not(composite(N)).
 table_nth(1, 2).
 nth_prime(N, P) :- table_nth(N, P), !.
 nth_prime(N, P) :- N1 is N - 1, nth_prime(N1, P1), NEXT is P1 + 2, next_prime(NEXT, P), assert(table_nth(N, P)), !.
-
 next_prime(OLD, NEW) :- composite(OLD), NEXT is OLD + 2, next_prime(NEXT, NEW), !.
 next_prime(OLD, NEW) :- OLD = NEW, !.
 
 sieve(N, CUR, LIMIT) :- SQUARE is CUR * CUR, sieve(N, CUR, SQUARE, LIMIT).
-sieve(N, CUR, SQUARE, LIMIT) :- SQUARE > LIMIT, !.
-sieve(N, CUR, SQUARE, LIMIT) :- composite(CUR), !, NEXT is CUR + 2, sieve(N, NEXT, LIMIT).
-sieve(N, CUR, SQUARE, LIMIT) :- assert(table_nth(N, CUR)), mark_comp(CUR, SQUARE, LIMIT), !,
-                       NEXT is CUR + 2, N1 is N + 1, sieve(N1, NEXT, LIMIT).
+sieve(_, _, SQUARE, LIMIT) :- SQUARE > LIMIT, !.
+sieve(N, CUR, _, LIMIT) :-
+    composite(CUR), !,
+    NEXT is CUR + 2, sieve(N, NEXT, LIMIT).
+sieve(N, CUR, SQUARE, LIMIT) :-
+    assert(table_nth(N, CUR)),
+    mark_comp(CUR, SQUARE, LIMIT), !,
+    NEXT is CUR + 2, N1 is N + 1, sieve(N1, NEXT, LIMIT).
 
 divisor(N, N) :- prime(N), !.
 
-mark_comp(P, SQUARE, LIMIT) :- composite(P), !.
+mark_comp(P, _, _) :- composite(P), !.
 mark_comp(P, SQUARE, LIMIT) :- DIFF is P + P, mark_comp(P, DIFF, SQUARE, LIMIT).
-mark_comp(P, DIFF, CUR, LIMIT) :- CUR > LIMIT, !.
-mark_comp(P, DIFF, CUR, LIMIT) :- assert(composite(CUR)), assert(divisor(CUR, P)),
-            NEXT is DIFF + CUR, mark_comp(P, DIFF, NEXT, LIMIT).
+mark_comp(_, _, CUR, LIMIT) :- CUR > LIMIT, !.
+mark_comp(P, DIFF, CUR, LIMIT) :-
+    assert(composite(CUR)), assert(divisor(CUR, P)),
+    NEXT is DIFF + CUR, mark_comp(P, DIFF, NEXT, LIMIT).
 
 prime_divisors(1, []) :- !.
-prime_divisors(N, [H | T]) :- number(N), divisor(N, H), N1 is div(N, H), prime_divisors(N1, T), !.
-prime_divisors(N, [H | T]) :- prime_divisors(N, [H | T], H), !.
-prime_divisors(1, [], _) :- !.
-prime_divisors(N, [H | T], MIN) :- H >= MIN, prime(H), prime_divisors(N1, T, H), N is N1 * H, !.
+prime_divisors(N, [H | T]) :-
+    number(N), divisor(N, H),
+    N1 is div(N, H), prime_divisors(N1, T), !.
+prime_divisors(N, [H | T]) :- prime_factors_list(N, [H | T], H), !.
+prime_factors_list(1, [], _) :- !.
+prime_factors_list(N, [H | T], MIN) :-
+    H >= MIN, prime(H),
+    prime_factors_list(N1, T, H), N is N1 * H, !.
 
 digit_list(0, K, []) :- !.
 digit_list(N, K, [H | T]) :- H is mod(N, K), R is div(N, K), digit_list(R, K, T).
